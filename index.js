7d4cb96a4831f3b0639a701a3729605514d48b82
luupanu <22541529+luupanu@users.noreply.github.com>
2022-03-10 23:12:18 +0200

    init catboard

---
+++ index.js
@@ -0,0 +1,146 @@
+/* WEB AUDIO API SETUP */
+
+// audio context
+const c = new AudioContext({ latencyHint: 'interactive', sampleRate: 48000 })
+
+// audio buffers to play soundfiles
+let audioBuffers = {}
+
+// master gain
+const masterGain = new GainNode(c, { gain: 0.5 })
+
+// dry signal
+const leftDry = new GainNode(c)
+const rightDry = new GainNode(c)
+
+// delayed signal
+const leftDelay = new DelayNode(c, { delayTime: 0.22, maxDelayTime: 2 })
+const rightDelay = new DelayNode(c, { delayTime: 0.3, maxDelayTime: 2 })
+
+const leftDelayFeedback = new GainNode(c, { gain: 0.5 })
+const rightDelayFeedback = new GainNode(c, { gain: 0.5 })
+
+const leftDelayGain = new GainNode(c, { gain: 0 })
+const rightDelayGain = new GainNode(c, { gain: 0 })
+
+leftDelay.connect(leftDelayFeedback)
+leftDelayFeedback.connect(leftDelay)
+
+rightDelay.connect(rightDelayFeedback)
+rightDelayFeedback.connect(rightDelay)
+
+leftDelay.connect(leftDelayGain)
+rightDelay.connect(rightDelayGain)
+
+// merged signal
+const merger = new ChannelMergerNode(c, { numberOfInputs: 2 })
+
+leftDry.connect(merger, 0, 0)
+leftDelayGain.connect(merger, 0, 0)
+rightDry.connect(merger, 0, 1)
+rightDelayGain.connect(merger, 0, 1)
+
+merger.connect(masterGain).connect(c.destination)
+
+/* OTHER SETUP */
+
+// keyboard controls
+const keyboard = 'asdfghjkl'
+
+// list of cat pics
+const catPics = [
+	'cat_1.jpg',
+	'cat_2.jpg',
+	'cat_3.jpg',
+	'cat_4.jpg',
+]
+
+// list of soundfiles
+const soundFiles = [
+	'catboard_1.mp3',
+	'catboard_2.mp3',
+	'catboard_3.mp3',
+	'catboard_4.mp3',
+]
+
+/* FUNCTIONS */
+
+async function loadSound(path) {
+	const xhr = await fetch(path)
+	const arrayBuffer = await xhr.arrayBuffer()
+	return c.decodeAudioData(arrayBuffer)
+}
+
+function playSound(soundFile) {
+	const source = new AudioBufferSourceNode(c, { buffer: audioBuffers[soundFile], channelCount: 1 })
+
+	source.connect(leftDry)
+	source.connect(rightDry)
+	source.connect(leftDelay)
+	source.connect(rightDelay)
+	source.start()
+
+	source.addEventListener('ended', event => {
+		source.disconnect()
+	})
+}
+
+function registerKey(key, sound) {
+	window.addEventListener('keydown', event => {
+		if (event.repeat)
+			return
+
+		if (event.key.toLowerCase() == key) {
+			const button = document.getElementById(key)
+			button.className += " active"
+			setTimeout(() => button.className = "play-button", 225)
+			button.click()
+		}
+	})
+}
+
+/* CONTROLS */
+const controls = {
+	'input-master': newValue => masterGain.gain.value = newValue,
+	'input-delaygain': newValue => {
+		leftDelayGain.gain.value = newValue
+		rightDelayGain.gain.value = newValue
+	},
+	'input-delayfeedback': newValue => {
+		leftDelayFeedback.gain.value = newValue
+		rightDelayFeedback.gain.value = newValue
+	},
+	'input-ldelaytime': newValue => leftDelay.delayTime.linearRampToValueAtTime(newValue, c.currentTime + 0.01),
+	'input-rdelaytime': newValue => rightDelay.delayTime.linearRampToValueAtTime(newValue, c.currentTime + 0.01),
+}
+
+/* ONLOAD */
+
+window.onload = async () => {
+	const buttonDiv = document.getElementById('buttons')
+
+	for (const [id, func] of Object.entries(controls)) {
+		const controller = document.getElementById(id)
+
+		controller.addEventListener('input', () => {
+			func(controller.value)
+		})
+	}
+
+	for (let i = 0; i < soundFiles.length; i++) {
+		audioBuffers[soundFiles[i]] = await loadSound(soundFiles[i])
+		const button = document.createElement('button')
+
+		button.innerText = `${keyboard[i]}`
+
+		button.setAttribute('onclick', `playSound("${soundFiles[i]}")`)
+		button.setAttribute('class', 'play-button')
+		button.setAttribute('style', `background-image: url("${catPics[i]}"); background-size: cover; background-repeat: no-repeat;`)
+		button.setAttribute('id', keyboard[i])
+
+		buttonDiv.appendChild(button)
+
+		registerKey(keyboard[i], soundFiles[i])
+	}
+}
+
